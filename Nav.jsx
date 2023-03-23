c45436dc65d7acf4a0f5cc533ebc28e9396e7506
AnaghaAravindh <127195155+AnaghaAravindh@users.noreply.github.com>
2023-03-23 12:09:53 +0530

    Add files via upload

--- Nav.jsx
+++ Nav.jsx
@@ -1,35 +1,54 @@
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <div>
-          
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-           
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align="left">
-          <Link to='/' style={{color:'white', textDecoration:'none'}}>React </Link>
-          </Typography>
-          <Button color="inherit" variant="text"><Link to='/students'
-           style={{color:'white', textDecoration:'none'}}>VEIW</Link></Button>
-          <Button color="inherit" variant="text"><Link to='/addstudents'
-           style={{color:'white', textDecoration:'none'}}>ADD</Link></Button>
-        </Toolbar>
-      </AppBar>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            ></IconButton>
+            {/* <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+              align="left"
+            >
+              <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+                React
+              </Link>
+            </Typography> */}
+            <Button color="inherit" variant="text">
+              <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+                VEIW
+              </Link>
+            </Button>
+            <Button color="inherit" variant="text">
+              <Link
+                to="/add"
+                style={{ color: "white", textDecoration: "none" }}>
+                ADD
+              </Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
       </Box>
-    </div> 
-  )
-}
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
--- Teachers.jsx
+++ Teachers.jsx
@@ -1,42 +1,53 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Teachers = () => {
-    var[Teachers,setTeachers]=useState([])
-   useEffect(()=>{
-    axios.get("http://localhost:3005/teachers")
-   .then(response=>{
-    console.log(response.data)
-    setTeachers(Teachers=response.data)
-   }) 
-   .catch(err=>console.log(err))
-   }) 
+  var [Teachers, setTeachers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3005/teachers")
+      .then((response) => {
+        console.log(response.data);
+        setTeachers((Teachers = response.data));
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
-      <Typography>   Welcome to my app</Typography>
+      <Typography> Welcome to my app</Typography>
       <TableContainer>
         <Table>
-            <TableHead>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Class</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Teachers.map((value, index) => {
+              return (
                 <TableRow>
-                <TableCell>Id</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Class</TableCell>
+                  <TableCell>{value.id}</TableCell>
+                  <TableCell>{value.name}</TableCell>
+                  <TableCell>{value.class}</TableCell>
                 </TableRow>
-            </TableHead>
-            <TableBody>
-                {Teachers.map((value,index)=>{
-                    return<TableRow>
-                        <TableCell>{value.id}</TableCell>
-                        <TableCell>{value.name}</TableCell>
-                        <TableCell>{value.class}</TableCell>
-                    </TableRow>
-                })}
-            </TableBody>
+              );
+            })}
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Teachers
+export default Teachers;
